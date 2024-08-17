b3bbbe463543df3994a8d4e7c75a26600dc66d01
Mingding Han <hanmd82@gmail.com>
2024-08-17 07:47:28 +0000

    Add base models and materializations

---
+++ models/base/stripe/base_stripe_order_payments.sql
@@ -0,0 +1,18 @@
+SELECT
+    id AS payment_id,
+    orderid AS order_id,
+    paymentmethod AS payment_method,
+    CASE
+        WHEN paymentmethod IN ('stripe', 'paypal', 'credit_card', 'gift_card')
+        THEN 'credit'
+        ELSE 'cash'
+    END AS payment_type,
+    status,
+    amount,
+    CASE
+        WHEN status = 'success'
+        THEN TRUE
+        ELSE FALSE
+    END AS is_payment_completed,
+    created AS created_date
+from `dbt-tutorial.stripe.payment`
