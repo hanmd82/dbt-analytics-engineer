affc93995a083dc3f5442c461b1b3026099586f2
Mingding Han <hanmd82@gmail.com>
2024-08-18 13:28:17 +0000

    Refactor to generate dynamic SQL with Jinja templates

--- models/intermediate/int_payment_type_amount_per_order.sql
+++ models/intermediate/int_payment_type_amount_per_order.sql
@@ -1,28 +1,35 @@
-WITH order_payments AS (
+{# declaration of payment_types variable. Add here if a new one appears #}
+{%- set payment_types = ['cash', 'credit'] -%}
+
+WITH payments AS (
     SELECT * FROM {{ ref('base_stripe_order_payments') }}
+),
+
+pivot_and_aggregate_payments_to_order_grain AS (
+    SELECT
+        order_id,
+        {% for payment_type in payment_types -%}
+            
+            
+            SUM(
+                CASE
+                    WHEN
+                        payment_type = '{{ payment_type }}'
+                        AND status = 'success'
+                        THEN amount
+                    ELSE 0
+                END
+            ) AS {{ payment_type }}_amount,
+        {%- endfor %}
+        SUM(
+            CASE
+                WHEN status = 'success'
+                    THEN amount
+            END
+        ) AS total_amount
+    FROM payments
+
+    GROUP BY 1
 )
 
-SELECT
-    order_id,
-    SUM(
-        CASE
-            WHEN payment_type = 'cash' AND status = 'success'
-                THEN amount
-            ELSE 0
-        END
-    ) AS cash_amount,
-    SUM(
-        CASE
-            WHEN payment_type = 'credit' AND status = 'success'
-                THEN amount
-            ELSE 0
-        END
-    ) AS credit_amount,
-    SUM(
-        CASE
-            WHEN status = 'success'
-                THEN amount
-        END
-    ) AS total_amount
-FROM order_payments
-GROUP BY 1
+SELECT * FROM pivot_and_aggregate_payments_to_order_grain
