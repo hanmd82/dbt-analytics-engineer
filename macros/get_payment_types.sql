7bfbfb04c132c166c0f4686fac8f933dfb890d5d
Mingding Han <hanmd82@gmail.com>
2024-08-18 13:41:59 +0000

    Refactor into query to get payment types from separate model

--- macros/get_payment_types.sql
+++ macros/get_payment_types.sql
@@ -1,3 +1,20 @@
 {% macro get_payment_types() %}
-    {{ return(['cash', 'credit']) }}
+    {% set payment_type_query %}
+    SELECT
+        DISTINCT payment_type
+    FROM {{ ref('base_stripe_order_payments') }}
+    ORDER by 1
+    {% endset %}
+
+    {% set results = run_query(payment_type_query) %}
+
+    {% if execute %}
+    {# Return the first column #}
+    {% set results_list = results.columns[0].values() %}
+    {% else %}
+    {% set results_list = [] %}
+    {% endif %}
+
+    {{ return(results_list) }}
+
 {% endmacro %}
